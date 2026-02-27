9be03a0fee9a2689de3eb3654d600eff2ef3417a
Rifki Fahriansyah <rifkifahriansyah_2327250093@mhs.mdp.ac.id>
2026-02-27 18:04:38 +0700

    clean local cache and disable https trick for troubleshooting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // 2. Force HTTPS if in production (Vercel)
-        if (config('app.env') === 'production') {
-            URL::forceScheme('https');
-        }
+        // if (config('app.env') === 'production') {
+        //     URL::forceScheme('https');
+        // }
     }
 }
