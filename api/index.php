c48356d873ebe89190a7632691b4e1413ff5132f
Rifki Fahriansyah <rifkifahriansyah_2327250093@mhs.mdp.ac.id>
2026-02-27 17:27:38 +0700

    deploy vercel

---
+++ api/index.php
@@ -0,0 +1,5 @@
+<?php
+
+// This file is the entry point for Vercel's serverless functions.
+// It simply forwards the request to Laravel's public entry point.
+require __DIR__ . '/../public/index.php';
