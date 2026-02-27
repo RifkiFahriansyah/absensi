f237eef5f8e6df644a41d2d59f9394c588e3d95a
Rifki Fahriansyah <rifkifahriansyah_2327250093@mhs.mdp.ac.id>
2026-02-27 17:59:57 +0700

    tes vercel public

--- api/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// This file is the entry point for Vercel's serverless functions.
-// It simply forwards the request to Laravel's public entry point.
-require __DIR__ . '/../public/index.php';
